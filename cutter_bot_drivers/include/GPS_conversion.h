2257674bbe558172008e9389ecce59b6bbb45e1b
EJ <ejk43@case.edu>
2012-11-09 17:22:38 -0500

    Added GPS_conversion node to cutter_bot_drivers. Added node to start_novatel.lauch

---
+++ cutter_bot_drivers/include/GPS_conversion.h
@@ -0,0 +1,83 @@
+/* Copyright (c) 2012, EJ Kreinar
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ ********************************************************************************
+ * GPS_conversion.h
+ *   Provides utitility function for GPS conversion (LLA to ENU) 
+ *
+ ********************************************************************************/
+#ifndef GPS_CONVERSION
+#define GPS_CONVERSION
+
+#include <ros/ros.h>
+#include <math.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/Point.h>
+
+namespace GPS_conversion{
+
+const double a  = 6378137;      // meters, Semi Major Axis
+const double b  = 6356752.3142; // meters, Semi Minor Axis
+const double e2 = 1-pow(b/a,2); // first eccentricity squared
+
+
+static void LLA2ENU(const sensor_msgs::NavSatFix &gps, const sensor_msgs::NavSatFix &ref, geometry_msgs::Point &enu)
+{
+  // Based off the paper by S.P. Drake: http://www.dsto.defence.gov.au/publications/2443/DSTO-TN-0432.pdf
+
+  // Location of reference point in radians
+  double phi = ref.latitude*M_PI/180;
+  double lam = ref.longitude*M_PI/180;
+  double h   = ref.altitude;
+
+  // Location of gps point in radians
+  double dphi = gps.latitude*M_PI/180 - phi;
+  double dlam = gps.longitude*M_PI/180 - lam;
+  double dh   = gps.altitude - h;
+  
+  // Constants
+  double tmp = sqrt(1-e2*pow(sin(phi),2));
+  double cl = cos(lam);
+  double sl = sin(lam);
+  double cp = cos(phi);
+  double sp = sin(phi);
+
+  // Transformations
+  double de = (a/tmp+h)*cp*dlam - (a*(1-e2)/(pow(tmp,3))+h)*sp*dphi*dlam + cp*dlam*dh;
+  double dn = (a*(1-e2)/pow(tmp,3) + h)*dphi + 1.5*cp*sp*a*e2*pow(dphi,2) + pow(sp,2)*dh*dphi + 0.5*sp*cp*(a/tmp+h)*pow(dlam,2);
+  double du = dh - 0.5*(a-1.5*a*e2*pow(cp,2)+0.5*a*e2+h)*pow(dphi,2) - 0.5*pow(cp,2)*(a/tmp-h)*pow(dlam,2);
+
+  enu.x = de;
+  enu.y = dn;
+  enu.z = du;
+  
+  return;
+}
+
+}
+
+
+#endif
---
+++ cutter_bot_drivers/src/convert_gps_to_pose.cpp
@@ -0,0 +1,202 @@
+/* Copyright (c) 2012, EJ Kreinar
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <organization> nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ ********************************************************************************
+ * convert_gps_to_pose.cpp
+ *   Converts a GPS NavSatFix message to a PoseWithCovarianceStamped
+ *   TODO: Also look at the velocity message and publish a TwistWithCovarianceStamped
+ * 
+ * Subscribes:
+ *   - gps_fix (sensor_msgs/NavSatFix): Current GPS reading
+ *   - odom (nav_msgs/Odometry): Odometry measurement
+ *  
+ * Publishes:
+ *   - state (cutter_msgs/State): Current state of the robot in the /map frame
+ *
+ * Broadcasts:
+ *   - map->odom transform: Odometry correction
+ *
+ * Parameters:
+ *   - ref_lat: Latitude reference point
+ *   - ref_lon: Longitude reference point
+ *   - rel_alt: Altitude reference point
+ *   - ~gps: Topic name of the subscribed NavSatFix message
+ *   - ~pose: Topic name of the published PoseWithCovarianceStamped message
+ *
+ ********************************************************************************/
+
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <ros/console.h>
+#include <math.h>
+#include "angles/angles.h"
+#include <sensor_msgs/NavSatFix.h>
+#include <tf/tf.h>
+#include "GPS_conversion.h"
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+
+class CutterGPSConversion
+{
+  public:
+    CutterGPSConversion();
+
+  private:
+    void gpsCallback(const sensor_msgs::NavSatFix& gps);
+    void velCallback(const geometry_msgs::TwistStamped& vel);
+    void publishState();
+    bool checkForMessages();
+
+    sensor_msgs::NavSatFix fix_;
+    geometry_msgs::TwistStamped vel_;
+    bool fix_rcv_;
+    bool vel_rcv_;
+  
+    ros::NodeHandle node_;
+    ros::Publisher  pose_pub_;
+    ros::Subscriber gps_sub_;
+    ros::Subscriber vel_sub_;
+ 
+    sensor_msgs::NavSatFix ref_;
+};
+
+CutterGPSConversion::CutterGPSConversion()
+{
+  // Get parameters
+  double lat, lon, alt;
+  if (!(node_.getParam("ref_lat",lat) &&
+        node_.getParam("ref_lon",lon) &&
+        node_.getParam("ref_alt",alt)))
+  {
+    // Provide a default reference point if there's no reference params
+    lat = 41.5012408976;
+    lon = -81.6063695197;
+    alt = 216;
+    ROS_WARN("Reference parameters not found (ref_lat, ref_lon, ref_alt). Using defaults");
+  }
+  ref_.latitude  = lat;
+  ref_.longitude = lon;
+  ref_.altitude  = alt;
+
+  // Get topic names to subscribe/publish
+  std::string gps, pose, vel;
+  if (!(ros::param::get("~gps",gps) &&
+        ros::param::get("~pose",pose) &&
+        ros::param::get("~vel",vel) ))
+  {
+    gps  = "gps_fix";
+    pose = "gps_pose"; 
+    vel  = "gps_vel";
+    ROS_WARN("Reference parameters not found (~gps, ~pose, ~vel). Using defaults");
+  }
+  
+  // Setup Publishers and subscribers
+  pose_pub_ = node_.advertise<geometry_msgs::PoseStamped>(pose,1);
+  gps_sub_ = node_.subscribe(gps, 1, &CutterGPSConversion::gpsCallback, this);
+  vel_sub_ = node_.subscribe(vel, 1, &CutterGPSConversion::velCallback, this);
+}
+
+void CutterGPSConversion::publishState()
+{
+  fix_rcv_ = false;
+  vel_rcv_ = false;
+  
+  geometry_msgs::Point point;
+  GPS_conversion::LLA2ENU(fix_, ref_, point);
+
+  ros::Time current_time = ros::Time::now();  
+
+  // Pose message: Setup header
+  geometry_msgs::PoseStamped pose_msg;
+  pose_msg.header.stamp = current_time; //TODO: Should I use the current time or use the time from the gps message??
+  pose_msg.header.frame_id = "map";
+
+  // Pose message: Set the pose
+  pose_msg.pose.position = point;
+
+  // Pose message: Set the orientation
+  double trackAngle = atan2(vel_.twist.linear.y, vel_.twist.linear.x);
+  //ROS_INFO("Xvel: %f, Yvel: %f, angle: %f", vel_.twist.linear.x, vel_.twist.linear.y, trackAngle);
+  pose_msg.pose.orientation = tf::createQuaternionMsgFromYaw(trackAngle);
+
+  // Pose message: Set the covariance
+  // TODO: Set the covariance according to the NavSatFix message
+
+  // Publish the state
+  pose_pub_.publish(pose_msg);
+  ROS_INFO("Published pose");
+}
+
+void CutterGPSConversion::gpsCallback(const sensor_msgs::NavSatFix& gps)
+{
+  fix_ = gps;
+  fix_rcv_ = true;
+  //ROS_INFO("Received gps fix");
+  if (checkForMessages())
+    publishState();
+}
+
+void CutterGPSConversion::velCallback(const geometry_msgs::TwistStamped& vel)
+{
+  vel_ = vel;
+  vel_rcv_ = true;
+  //ROS_INFO("Received gps velocity");
+  if (checkForMessages())
+    publishState();
+}
+
+bool CutterGPSConversion::checkForMessages()
+{
+  if (fix_rcv_ && vel_rcv_) // Check if we have both messages
+    return (fix_.header.stamp - vel_.header.stamp) < ros::Duration(.5); // Compare the time stamps
+  else
+    return false;
+}
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "localization");
+  CutterGPSConversion conversion;
+  ros::spin();
+/*
+  while (ros::ok())
+  {
+    if (conversion.fix_rcv_ && conversion.vel_rcv_)
+    {
+      if ( (conversion.fix_.header.stamp - conversion.vel_.header.stamp) < ros::Duration(.5) )
+      {
+        ROS_INFO("WHOOOOOOOOOOOOOOOOOOOOOO!!!!!");
+        conversion.fix_rcv_ = false;
+        conversion.vel_rcv_ = false;
+      }
+    }
+    ros::spinOnce();  
+  }
+*/
+  return 0;
+}
