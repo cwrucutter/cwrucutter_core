acd04e7758774c6d0f347a18a9e38e6c42739dac
EJ <ejk43@case.edu>
2012-12-12 15:07:19 -0500

    Kalman filter tracks the wheel velocity!

--- cutter_localization/src/slip_detect.cpp
+++ cutter_localization/src/slip_detect.cpp
@@ -42,6 +42,7 @@
 #include "sensor_msgs/Imu.h"
 #include "math.h"
 #include "cutter_msgs/Testing.h"
+#include <vector>
 
 // RunningStat class source code from the helpful website:
 // http://www.johndcook.com/standard_deviation.html
@@ -109,48 +110,65 @@ class SlipDetect
 public:
   SlipDetect();
   void filter();
+  bool initialize();
+  bool lookupParams();
   void setLoopRate(double rate) { loop_rate_ = rate; };
 
 private:
+  // Ros Classes
   ros::Subscriber odom_sub_;
   ros::Subscriber imu_sub_;
   ros::Publisher test_pub_;
   ros::NodeHandle nh_;
+  
   RunningStat imu_stat_x_;
   RunningStat imu_stat_y_;
 
-  nav_msgs::Odometry last_odom_;
-  sensor_msgs::Imu   last_imu_;
 
   double loop_rate_;
-  
-  double odom_v_;
+ 
+  // Store odom and imu values from callbacks
+  nav_msgs::Odometry last_odom_;
+  sensor_msgs::Imu   last_imu_;
+  /*double odom_v_;
   double odom_w_;
   double imu_w_;
   double imu_x_;
-  double imu_y_;
-  
+  double imu_y_;*/
   bool imu_new_;
   bool odom_new_;
+  
+  // Initialization parameters
+  double odom_var_v_;
+  double odom_var_w_;
+  double imu_var_a_;
+  double imu_var_w_;
+  double proc_var_v_;
+  double proc_var_w_;
+  double proc_var_a_;
+  double proc_var_wdot_;
+  
+  // Kalman Filter Variables
+  std::vector<double> state_;
+  std::vector<double> cov_;
+  
 
   void odomCallback(const nav_msgs::Odometry::ConstPtr& odom) 
   {
-    odom_v_ = odom->twist.twist.linear.x;
-    odom_w_ = odom->twist.twist.angular.z;
+    last_odom_ = *odom;
     odom_new_ = true;
   };
 
   void imuCallback(const sensor_msgs::Imu::ConstPtr& imu)
   {
-    imu_w_ = imu->angular_velocity.z;
-    imu_x_ = imu->linear_acceleration.x;
-    imu_y_ = imu->linear_acceleration.y; 
+    last_imu_ = *imu;
     imu_new_ = true;
   };
 
 };
 
-SlipDetect::SlipDetect()
+SlipDetect::SlipDetect():
+  state_(4,0), cov_(16,0)
 {
   odom_new_ = false;
   imu_new_  = false;
@@ -159,34 +177,162 @@ SlipDetect::SlipDetect()
   test_pub_ = nh_.advertise<cutter_msgs::Testing>("cwru/testing",1);
 };
 
-void SlipDetect::filter()
+bool SlipDetect::lookupParams()
 {
-  if (imu_new_)
-  {
-    if (abs(odom_v_) < .001 && abs(odom_w_ < .001))
-    {
-      // Filter the IMU when the wheels report no motion (assume the robot is not moving) 
-      imu_stat_x_.Push(imu_x_);
-      imu_stat_y_.Push(imu_y_);
-      ROS_INFO("Imu X: %f, X average: %f, Stddev: %f, X-avg: %f", 
-                    imu_x_,  imu_stat_x_.Mean(), imu_stat_x_.StandardDeviation(), imu_x_-imu_stat_x_.Mean());
-      ROS_INFO("Imu Y: %f, Y average: %f, Stddev: %f, Y-avg: %f", 
-                    imu_y_,  imu_stat_y_.Mean(), imu_stat_y_.StandardDeviation(), imu_y_-imu_stat_y_.Mean());
-    }
-    imu_new_ = false;
-  }
+  bool rval = ros::param::get("~odom_var_v", odom_var_v_)
+           && ros::param::get("~odom_var_w", odom_var_w_)
+           && ros::param::get("~imu_var_a",  imu_var_a_)
+           && ros::param::get("~imu_var_w",  imu_var_w_)
+           && ros::param::get("~proc_var_v",  proc_var_v_)
+           && ros::param::get("~proc_var_w",  proc_var_w_)
+           && ros::param::get("~proc_var_a",  proc_var_a_)
+           && ros::param::get("~proc_var_wdot",  proc_var_wdot_);
+  return rval;
+}
+
+bool SlipDetect::initialize()
+{  
+  cov_[0]  = 1;
+  cov_[5]  = 1;
+  cov_[10] = 1;
+  cov_[15] = 1;
+  
+  return true;
+}
+
+void SlipDetect::filter()
+{  
+  double odom_v, odom_w, imu_x, imu_y, imu_w;
+  odom_v = last_odom_.twist.twist.linear.x;
+  odom_w = last_odom_.twist.twist.angular.z;
+  imu_x  = last_imu_.linear_acceleration.x;
+  imu_y  = last_imu_.linear_acceleration.y;
+  imu_w  = last_imu_.angular_velocity.z;
   
   double x_accel, y_accel;
+  x_accel = imu_x - imu_stat_x_.Mean();
+  y_accel = imu_y - imu_stat_y_.Mean();
   
-  x_accel = imu_x_ - imu_stat_x_.Mean();
-  y_accel = imu_y_ - imu_stat_y_.Mean();
+  double cov_v, cov_w, odom_v_err, odom_w_err = 0;
+
+  if (imu_new_)
+  {
+    if (fabs(odom_v) < .001 && fabs(odom_w) < .001)
+    {
+      // Filter the IMU when the wheels report no motion (assume the robot is not moving) 
+      imu_stat_x_.Push(imu_x);
+      imu_stat_y_.Push(imu_y);
+      ROS_INFO("Imu X: %f, X average: %f, Stddev: %f, X-avg: %f", 
+                    imu_x,  imu_stat_x_.Mean(), imu_stat_x_.StandardDeviation(), imu_x-imu_stat_x_.Mean());
+      ROS_INFO("Imu Y: %f, Y average: %f, Stddev: %f, Y-avg: %f", 
+                    imu_y,  imu_stat_y_.Mean(), imu_stat_y_.StandardDeviation(), imu_y-imu_stat_y_.Mean());
+      ROS_INFO("Odom V: %f, Odom W: %f, fabs(odomv): %f, fabs(odomw): %f", odom_v, odom_w, fabs(odom_v), fabs(odom_w));
+    }
+    else
+    {
+      // Currently do nothing with the IMU if we're moving??
+      ROS_INFO("imu: No motion");
+    }
+    
+  } 
   
+  if (odom_new_)
+  {
+    if (fabs(odom_v) < .001 && fabs(odom_w) < .001)
+    {
+      // Currently do nothing with odometry if we're not moving??
+      ROS_INFO("odom: No motion");
+    }
+    else
+    {
+      // When the robot is moving, implement the kalman filter thing..
+      //double state_pre[4], state_post[4], cov_pre[16], cov_post[16];
+      std::vector<double> state_pre(4,0);
+      std::vector<double> cov_pre(16,0);
+      double odom_std_v, odom_std_w;
+      double dt = 1/loop_rate_;
+      
+      //ROS_INFO("Initial State: [ %f, %f, %f, %f]", state_[0], state_[1], state_[2], state_[3]);
+      // Propagate the state to X_pre
+      state_pre[0] = state_[0] + state_[2]*dt;  // v_new = v + a*dt
+      state_pre[1] = state_[1] + state_[3]*dt;  // w_new = w + w_dot*dt
+      state_pre[2] = state_[2];                // a_new = a
+      state_pre[3] = state_[3];                // w_dot_new = w_dot
+      //ROS_INFO("State Pre:  [ %f, %f, %f, %f]", state_pre[0], state_pre[1], state_pre[2], state_pre[3]);
+      
+      // Propagate Covariance: Diagonal elements only
+      /*cov_pre[0]  = cov_[0] + cov_[10]*dt*dt;
+      cov_pre[5]  = cov_[5] + cov_[15]*dt*dt;
+      cov_pre[10] = cov_[10];
+      cov_pre[15] = cov_[15];*/
+      
+      // Propagate Covariance to P_pre (after state update): All elements
+      //ROS_INFO("Initial Cov Diag: [ %f, %f, %f, %f]", cov_[0], cov_[5], cov_[10], cov_[15]);
+      cov_pre[0]  = cov_[0] + cov_[8]*dt  + cov_[2]*dt + cov_[10]*dt*dt + proc_var_v_*dt;
+      cov_pre[2]  = cov_[2] + cov_[10]*dt;
+      cov_pre[5]  = cov_[5] + cov_[13]*dt + cov_[7]*dt + cov_[15]*dt*dt + proc_var_w_*dt;
+      cov_pre[7]  = cov_[7] + cov_[15]*dt;
+      cov_pre[8]  = cov_[10]*dt;
+      cov_pre[10] = cov_[10] + proc_var_a_*dt;
+      cov_pre[13] = cov_[15]*dt;
+      cov_pre[15] = cov_[15] + proc_var_wdot_*dt;
+      //ROS_INFO("Cov Diag Pre: [ %f, %f, %f, %f]", cov_pre[0], cov_pre[5], cov_pre[10], cov_pre[15]);
+      
+      // Odometry residual Standard Deviation
+      odom_std_v = sqrt(cov_pre[0] + odom_var_v_);
+      odom_std_w = sqrt(cov_pre[5] + odom_var_w_);
+      
+      // Propagate Covariance to P+ (after measurement update): All elements
+      // Encoder update only
+      cov_v = cov_pre[0] + odom_var_v_;
+      cov_w = cov_pre[5] + odom_var_w_;
+      cov_[0]  = cov_pre[0] - cov_pre[0] * cov_pre[0] / cov_v; 
+      cov_[2]  = cov_pre[2] - cov_pre[0] * cov_pre[2] / cov_v; 
+      cov_[5]  = cov_pre[5] - cov_pre[5] * cov_pre[5] / cov_w; 
+      cov_[7]  = cov_pre[7] - cov_pre[5] * cov_pre[7] / cov_w; 
+      cov_[8]  = cov_pre[8] - cov_pre[8] * cov_pre[0] / cov_v; 
+      cov_[10] = cov_pre[10]- cov_pre[8] * cov_pre[2] / cov_v; 
+      cov_[13] = cov_pre[13]- cov_pre[13]* cov_pre[5] / cov_w; 
+      cov_[15] = cov_pre[15]- cov_pre[13]* cov_pre[7] / cov_w; 
+      
+      // Calculate odometry residual
+      odom_v_err = odom_v - state_pre[0];
+      odom_w_err = odom_w - state_pre[1];
+      
+      // Measurement update
+      state_[0] = state_pre[0] + odom_v_err * cov_[0] / odom_var_v_;
+      state_[1] = state_pre[1] + odom_w_err * cov_[5] / odom_var_w_;
+      state_[2] = state_pre[2] + odom_v_err * cov_[8] / odom_var_v_;
+      state_[3] = state_pre[3] + odom_w_err * cov_[13]/ odom_var_w_;    
+      
+      //ROS_INFO("Cov_v: %f, Cov_w: %f, Odom_err_v: %f, odom_err_w: %f", cov_v, cov_w, odom_v_err, odom_w_err);
+      //ROS_INFO("odom_var_v: %f, odom_var_w: %f", odom_var_v_, odom_var_w_);
+      //ROS_INFO("Cov For Meas Update: [ %f, %f, %f, %f]", cov_[0], cov_[5], cov_[8], cov_[13]);
+      ROS_INFO("State Post: [ %f, %f, %f, %f]", state_[0], state_[1], state_[3], state_[4]);
+      ROS_INFO("Cov Diag Post: [ %f, %f, %f, %f]", cov_[0], cov_[5], cov_[10], cov_[15]);
+      ROS_INFO("Odom meas: V: %f, W: %f", odom_v, odom_w);
+      
+      //state_ = state_post;
+      //cov_   = cov_post;
+    }
+  }
+  
+  imu_new_ = false;
+  odom_new_ = false;
+    
   cutter_msgs::Testing test;
   test.corrected_imu_x = x_accel;
   test.corrected_imu_y = y_accel;
+  test.kf_v = state_[0];
+  test.kf_w = state_[1];
+  test.kf_a = state_[2];
+  test.kf_wdot = state_[3];
+  test.innov_bound_v = 3*sqrt(cov_v); // bound on innovation is 3*sigma(v)
+  test.innov_bound_w = 3*sqrt(cov_w); // bound on innovation is 3*sigma(w)
+  test.innov_v = odom_v_err;
+  test.innov_w = odom_w_err;
+
   test_pub_.publish(test);
-  
-  // When the robot is moving, implement the kalman filter thing, evaluate the feasibility..
   
 };
 
@@ -195,6 +341,18 @@ int main(int argc, char ** argv)
 {
   ros::init(argc, argv, "slip_detect"); //Init ROS
   SlipDetect detector; //Construct class
+
+  if (!detector.lookupParams())
+  {
+    ROS_ERROR("Parameters not found");
+    return 0;
+  }
+    
+  if (!detector.initialize())
+  {
+    ROS_ERROR("Could not initialize filter");
+    return 0;
+  }
 
 //  ros::spin(); // Run until interupted 
   double looprate = 20;
