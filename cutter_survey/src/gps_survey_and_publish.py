b1ca08870d900cecba2d24821a8e4a65714b0d74
EJ <ejk43@case.edu>
2013-02-26 17:05:08 -0500

    Added new survey message, and new gps_survey procedure (checking in new files)

---
+++ cutter_survey/src/gps_survey_and_publish.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, EJ Kreinar
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the names of the authors nor the names of their
+#    affiliated organizations may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib
+roslib.load_manifest('cutter_survey')
+import rospy
+import sys
+import collections
+
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Point
+from cutter_msgs.msg import Survey
+from std_srvs.srv import Empty
+
+# syntax to start the node: 
+#   rosrun cutter_survey gps_survey.py _filename:=filenamehere
+
+class Surveyor:
+    def __init__(self):
+        # Start the node
+        rospy.init_node('gps_surveyor_publisher')
+        topic_in  = rospy.get_param('~topic_in','/gps_pose')
+        topic_out = rospy.get_param('~topic_out','/cwru/survey')
+        filename = rospy.get_param('~filename','survey.txt')
+        window_size = rospy.get_param('~window_size',10) # magic number 10 would be (GPScallbackHz)*(windowlengthinseconds), assuming 10Hz and 1-second window
+        self.file = roslib.packages.get_pkg_dir('cutter_survey')+'/survey/'+filename
+
+        rospy.loginfo('Subscribing to: '+topic_in)
+        rospy.loginfo('Publishingg to: '+topic_out)
+        rospy.loginfo('Output file will be: '+ self.file)
+
+        # Initialize
+        self.coords = (0.0, 0.0, 0.0)
+        self.average = (0.0, 0.0, 0.0)
+        self.history = collections.deque(maxlen=window_size)
+        self.surveyPts = Survey()
+
+        # Start the ROS stuff
+        rospy.Subscriber(topic_in, PoseStamped, self.GPSCallback)
+        rospy.Service('survey_gps_point', Empty, self.SurveyServer)
+        self.pub = rospy.Publisher(topic_out, Survey)
+
+        rate = rospy.Rate(1.0)
+        while not rospy.is_shutdown():
+            self.pub.publish(self.surveyPts)
+            rate.sleep()
+
+        
+    def GPSCallback(self, data):
+        # GPS Calback: Called when a new gps point arrives
+        self.coords = (data.pose.position.x, data.pose.position.y, data.pose.position.z)
+        self.history.append(self.coords)
+        self.average = (mean(map(lambda x: x[0], self.history)), mean(map(lambda y: y[1], self.history)), mean(map(lambda z: z[2], self.history))) 
+        sys.stdout.write("Current GPS point: (%f, %f, %f) \t\t Average point: (%f, %f, %f)\r" \
+			                    % (data.pose.position.x, data.pose.position.y, data.pose.position.z,  \
+			                    self.average[0], self.average[1], self.average[2])) 
+        sys.stdout.flush()
+
+    def SurveyServer(self, req):
+        # Survey Server: Called to record the point
+        #rospy.loginfo("Surveying... LastPoint: %s ", ('%f, %f, %f' % self.coords) )
+        pt = Point()
+        pt.x = self.average[0]
+        pt.y = self.average[1]
+        pt.z = self.average[2]
+        self.surveyPts.points.append(pt)
+        rospy.loginfo("Surveying... AveragePoint: %s ", ('%f, %f, %f' % self.average) )
+        with open(self.file,'a') as f:
+            #write_data = f.write(str('%f, %f, %f\n' % self.coords))
+            write_data = f.write(str('%f, %f, %f\n' % self.average))
+        return []
+
+def mean(l):
+    return float(sum(l))/len(l) if len(l) > 0 else float('nan')
+
+if __name__ == "__main__":
+    try:
+        Surveyor()
+    except rospy.ROSInterruptException: pass
+
---
+++ cutter_survey/src/publish_survey_from_txtfile.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+# EJ Kreinar
+# Copyright (c) 2013
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the names of the authors nor the names of their
+#    affiliated organizations may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib
+roslib.load_manifest('cutter_survey')
+import rospy
+import tf
+import math
+from geometry_msgs.msg import Point
+from cutter_msgs.msg import Survey
+
+class SnowmapTransformer:
+    def __init__(self):
+        # Start the node
+        rospy.init_node('recorded_survey_publisher')
+        surveyfile = rospy.get_param('~file_survey','survey.txt')
+        topic_out = rospy.get_param('~topic_out','/cwru/survey')
+        self.file_survey = roslib.packages.get_pkg_dir('cutter_survey')+'/survey/'+surveyfile
+        
+        rospy.loginfo('Survey file will be:\n '+ self.file_survey)
+        
+        self.pts_survey = []  # List to contain surveyed points
+        self.surveyPts = Survey()
+        
+        self.ReadFiles()
+        
+        self.pub = rospy.Publisher(topic_out, Survey)
+
+        rate = rospy.Rate(1.0)
+        while not rospy.is_shutdown():
+            self.pub.publish(self.surveyPts)
+            rate.sleep()
+
+        
+    
+    def ReadFiles(self):
+        # ReadFiles function reads the survey file and the snowmap file to extract coordinates
+        f_survey = open(self.file_survey,'r')
+        
+        # Extract csv
+        for line in f_survey:                  # iterates through each line in the file
+            res = map(float,line.split(','))   # parses the csv
+            self.pts_survey.append(res)        # appends to the list
+            pt = Point()
+            pt.x = res[0]
+            pt.y = res[1]
+            pt.z = res[2]
+            self.surveyPts.points.append(pt)
+        f_survey.close()
+        
+        # Print points
+        print "\nSurveyed Points:"
+        print self.pts_survey
+        
+
+if __name__ == "__main__":
+    try:
+        SnowmapTransformer()
+    except rospy.ROSInterruptException: pass
+
